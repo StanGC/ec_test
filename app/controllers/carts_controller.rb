370f5295db24d59c5ca8dec14057e8d3b93e6148
Stan Chen <solve153@gmail.com>
2019-02-02 15:20:32 +0800

    Cart can create orders

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,14 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!, only: [:checkout]
+
   def clean
     current_cart.clean!
     flash[:warning] = '已清空購物車'
 
     redirect_to carts_path
+  end
+
+  def checkout
+    @order = Order.new
   end
 end
