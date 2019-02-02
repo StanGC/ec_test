7ef27502db21bd366317b8a1e437a8fb59b2ca7c
Stan Chen <solve153@gmail.com>
2019-02-02 14:07:22 +0800

    Implement product index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
-  root 'welcome#index'
+  root 'products#index'
+
+  resources :products
 end
