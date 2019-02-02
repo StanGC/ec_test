be4d47fe4b445819b3d7a52365ec451e22054645
Stan Chen <solve153@gmail.com>
2019-02-02 15:22:49 +0800

    Add model product_list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_070756) do
+ActiveRecord::Schema.define(version: 2019_02_02_072107) do
 
   create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "cart_id"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_02_02_070756) do
     t.string "billing_address"
     t.string "shipping_name"
     t.string "shipping_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "order_id"
+    t.string "product_name"
+    t.decimal "product_price", precision: 10
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
