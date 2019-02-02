4e14d40194c1ebe667f3e47c744ecc75d865f8ea
Stan Chen <solve153@gmail.com>
2019-02-02 13:44:50 +0800

    Add user & product data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  id: 1,
+  email: 'test@test.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+10.times do
+  title = Faker::Lorem.word
+  attrs = {
+    description: Faker::Lorem.paragraph,
+    quantity: Faker::Number.number(3).to_i,
+    price: Faker::Number.number(4).to_i,
+  }
+  Product.create_with(attrs).find_or_create_by(title: title)
+end
