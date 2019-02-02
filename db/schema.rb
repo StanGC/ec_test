0a5ca077442fbb34f9fcb7d623384d2680244785
Stan Chen <solve153@gmail.com>
2019-02-02 15:29:15 +0800

    Add column token to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_072107) do
+ActiveRecord::Schema.define(version: 2019_02_02_072826) do
 
   create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "cart_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_02_02_072107) do
     t.string "shipping_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   create_table "product_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
